fb3c24916e8e3f39108871e60ffce2d0f700399b
Jesse Kooyenga <jesskooyenga@gmail.com>
2026-03-07 09:34:29 -0500

    updated api call functions

--- src/utils/spotifyAPI.js
+++ src/utils/spotifyAPI.js
@@ -1,52 +1,49 @@
-export async function getSpotifyToken() {
-    const clientId=import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-    const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 
+export async function getSpotifyToken() {
+    const authString = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+    
     const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${authString}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: new URLSearchParams({
-            grant_type: 'client_credentials',
-            client_id: clientId,
-            client_secret: clientSecret,
-        })
+        body: 'grant_type=client_credentials'
     });
 
     if (!response.ok) {
-        throw new Error(`Token fetch failed: ${response.statusText}`);
+        throw new Error('Failed to fetch Spotify token');
     }
 
     const data = await response.json();
     return data.access_token;
 }
 
-//fetch function to get albums
 export async function getNewReleases() {
     const token = await getSpotifyToken();
-
-    const response = await fetch('https://api.spotify.com/v1/browse/new-releases?limit=10', {
+  
+    const response = await fetch(
+      'https://api.spotify.com/v1/browse/new-releases?limit=20&market=US',
+      {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${token}`
+          Authorization: `Bearer ${token}`
         }
-    });
-
-    if(!response.ok) {
-        throw new Error(`Album fetch failed: ${response.stausText}`);
-    }
-
+      }
+    );
+  
     const data = await response.json();
+  
+    console.log("Spotify API data:", data.albums.items);
+  
     return data.albums.items;
-}
-
+  }
 export async function getAlbumById(albumId) {
     const token = await getSpotifyToken();
-
-    const requestUrl = "https://api.spotify.com/v1/albums/" + albumId;
-
-    const response = await fetch(requestUrl, {
+    
+    const response = await fetch('https://api.spotify.com/v1/albums/' + albumId, {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${token}`
